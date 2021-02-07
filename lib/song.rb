4c6c88c40d33cb42c3a444c87c3bd421f97fb69c
Vanessa Fiechtner <fiechtnervanessa@gmail.com>
2021-02-07 21:23:05 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,18 +1,19 @@
-class Song
-  extend Concerns::Findable
-  extend Memorable::ClassMethods
-  include Memorable::InstanceMethods
+require "pry"
 
+class Song
   attr_accessor :name
   attr_reader :artist, :genre
-
   @@all = []
 
-  def initialize(name, artist = nil, genre = nil)
+  def initialize(name, artist=nil, genre=nil)
     @name = name
-    self.artist=(artist) if artist
-    self.genre=(genre) if genre
-    save
+
+    if artist != nil
+      self.artist = artist
+    end
+    if genre != nil
+      self.genre = genre
+    end
   end
 
   def self.all
